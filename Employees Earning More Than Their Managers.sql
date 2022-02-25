ea748e7c64b23c15c710327933655dd8ed4eea0b
tyt51685168 <tyt51685168@gmail.com>
2022-02-25 11:56:18 +0800

    Create Employees Earning More Than Their Managers.sql

---
+++ Employees Earning More Than Their Managers.sql
@@ -0,0 +1,29 @@
+/*
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| salary      | int     |
+| managerId   | int     |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table indicates the ID of an employee, their name, salary, and the ID of their manager.
+ 
+
+Write an SQL query to find the employees who earn more than their managers.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+*/
+
+-- My Answer
+
+SELECT a.name as Employee
+FROM Employee a
+LEFT JOIN Employee b
+ON a.managerId = b.id
+WHERE (a.salary - b.salary) > 0
+
+-- other solutions
