7b0b73b68e31b743b704772de4476f2f726397fe
tyt51685168 <tyt51685168@gmail.com>
2022-02-25 12:13:21 +0800

    Create Find Duplicate.sql

---
+++ Find Duplicate.sql
@@ -0,0 +1,35 @@
+/*
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| email       | varchar |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table contains an email. The emails will not contain uppercase letters.
+ 
+
+Write an SQL query to report all the duplicate emails.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+*/
+
+-- My Solution
+
+SELECT email
+FROM Person
+GROUP BY email
+HAVING COUNT(email) > 1
+
+-- other solutions (sub query)
+
+select Email from
+(
+  select Email, count(Email) as num
+  from Person
+  group by Email
+) as statistic
+where num > 1
+;
