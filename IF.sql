2f75fd11fb977f702d31d57a286b68152c6aa1b7
tyt51685168 <tyt51685168@gmail.com>
2022-04-11 12:02:36 +0800

    Create IF.sql

---
+++ IF.sql
@@ -0,0 +1,22 @@
+Table: Employees
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| employee_id | int     |
+| name        | varchar |
+| salary      | int     |
++-------------+---------+
+employee_id is the primary key for this table.
+Each row of this table indicates the employee ID, employee name, and salary.
+ 
+
+Write an SQL query to calculate the bonus of each employee. The bonus of an employee is 100% of their salary if the ID of the employee is an odd number and the employee name does not start with the character 'M'. The bonus of an employee is 0 otherwise.
+
+Return the result table ordered by employee_id.
+
+# 這裡跟 COALESCE.sql 的題目一樣，只是用 IF 寫得更精簡，執行速度也更快
+# 紀錄 IF 的用法，IF( 條件式, if_true_value, if_false_value )
+
+SELECT employee_id, IF(employee_id%2 = 1 AND name NOT LIKE 'M%', salary, 0) AS bonus
+FROM Employees
